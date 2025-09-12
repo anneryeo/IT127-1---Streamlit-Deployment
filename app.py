fd79cbf7fcdfce771623a09662d41580032fd460
Anne Reyes <reyes.christinejulliane@gmail.com>
2025-09-12 23:19:40 +0800

    updated gitignore

--- app.py
+++ app.py
@@ -9,15 +9,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, roc_curve, accuracy_score, classification_report, auc, precision_recall_curve
 from sklearn.metrics import precision_score, recall_score
 
-'''
-Run (as regular Python script): & "D:/Developer/Projects/Academic Projects/IT127-1 - Streamlit Deployment/.venv/Scripts/python.exe" "D:/Developer/Projects/Academic Projects/IT127-1 - Streamlit Deployment/app.py"
+# Run (as regular Python script): 
+# & "D:/Developer/Projects/Academic Projects/IT127-1 - Streamlit Deployment/.venv/Scripts/python.exe" "D:/Developer/Projects/Academic Projects/IT127-1 - Streamlit Deployment/app.py"
 
-Run (as Streamlit web app): & "D:/Developer/Projects/Academic Projects/IT127-1 - Streamlit Deployment/.venv/Scripts/python.exe" -m streamlit run "D:/Developer/Projects/Academic Projects/IT127-1 - Streamlit Deployment/app.py"
-'''
+# Run (as Streamlit web app): 
+# & "D:/Developer/Projects/Academic Projects/IT127-1 - Streamlit Deployment/.venv/Scripts/python.exe" -m streamlit run "D:/Developer/Projects/Academic Projects/IT127-1 - Streamlit Deployment/app.py"
 
 def main():
     st.title("Binary Classification Web App")
     st.sidebar.title("Binary Classification Web App")
+    st.markdown
 
 if __name__ == "__main__":
     main()
